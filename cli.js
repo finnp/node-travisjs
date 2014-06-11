31df9d1a5629a7360bb1773ae14044c3bf81a116
Finn <derfinn@gmail.com>
2014-06-11 22:07:38 +0200

    yml

--- cli.js
+++ cli.js
@@ -1,63 +1,71 @@
+#!/usr/bin/env node
 var ghauth = require('ghauthprompt');
 var request = require('request');
 var Configstore = require('configstore');
 var ghslug = require('github-slug');
+var yaml = require('yamljs');
+var fs = require('fs');
 
 var travisUrl = 'https://api.travis-ci.org';
 var userAgent = 'travis-init.js';
-
-var authOpts = {
-  scopes: [
-    "read:org",
-    "user:email",
-    "repo_deployment",
-    "repo:status",
-    "write:repo_hook"
-  ],
-  userAgent: userAgent,
-  note: userAgent
-}
 
 var travisHeaders = {
   'User-Agent': userAgent,
   'Accept': 'application/vnd.travis-ci.2+json'
 };
 
-getToken(function (token) {
-  travisHeaders.Authorization = 'token ' + token;
-  // get slug
-  ghslug('./', function (err, slug) {
-    if(err) return console.error(err);
-      console.log('Activating hook for ' + slug + '...');
-      var req = {};
-      req.url = travisUrl  + '/repos/' + slug;
-      req.headers = travisHeaders;
-      // get id for the repo
-      request.get(req, function (err, response, data) {
-        if(err) return console.error(err);
-        var hookId = JSON.parse(data).repo.id;
-        // turn on the hook
+function activateHook() {
+  getToken(function (token) {
+    travisHeaders.Authorization = 'token ' + token;
+    // get slug
+    ghslug('./', function (err, slug) {
+      if(err) return console.error(err);
+        console.log('Activating hook for ' + slug + '...');
         var req = {};
-        req.url = travisUrl + '/hooks';
+        req.url = travisUrl  + '/repos/' + slug;
         req.headers = travisHeaders;
-        req.json = {
-          hook: {
-            id: hookId,
-            active: true
-          }
-        }
-        request.put(req, function (err, response, data) {
+        // get id for the repo
+        request.get(req, function (err, response, data) {
           if(err) return console.error(err);
           if(response.statusCode !== 200)
             return console.error('Error' + response.statusCode);
-          console.log('Activated hook for ' + slug);
+          var hookId = JSON.parse(data).repo.id;
+          // turn on the hook
+          var req = {};
+          req.url = travisUrl + '/hooks';
+          req.headers = travisHeaders;
+          req.json = {
+            hook: {
+              id: hookId,
+              active: true
+            }
+          }
+          request.put(req, function (err, response, data) {
+            if(err) return console.error(err);
+            if(response.statusCode !== 200)
+              return console.error('Error' + response.statusCode);
+            console.log('Activated hook for ' + slug);
+          });
         });
-      });
-      
-  });
-});
+        
+    });
+  });  
+}
+
 
 function getToken(cb) {
+  var authOpts = {
+    scopes: [
+      "read:org",
+      "user:email",
+      "repo_deployment",
+      "repo:status",
+      "write:repo_hook"
+    ],
+    userAgent: userAgent,
+    note: userAgent
+  }
+  
   var config = new Configstore('travis-init');
   var token = config.get('token');
   if(token) {
@@ -85,3 +93,60 @@ function getToken(cb) {
   }
 }
 
+function generateBadge() {
+  ghslug('./', function (err, slug) {
+    console.log('[![Build Status](https://travis-ci.org/' + slug 
+      + '.svg?branch=master)](https://travis-ci.org/' + slug + ')'
+    );
+  });
+}
+
+function createYML() {
+  var opts = {
+    language: 'node_js',
+    node_js: ['0.10', '0.8']
+  }
+  var yml = yaml.stringify(opts, 10);
+  console.log('Writing .travis.yml:');
+  console.log(yml);
+  fs.writeFileSync('./.travis.yml', yml);
+}
+
+// CLI parser
+
+var parser = require('nomnom')
+  .script('travisjs')
+  ;
+  
+parser.command('init')
+  .help('initialize travis (hook and yml)')
+  .callback(function (opts) {
+      createYML();
+      // git add and commit ?
+      activateHook();
+  })
+  ;
+  
+parser.command('badge')
+  .help('generate badge')
+  .callback(function (opts) {
+      generateBadge();
+  })
+  ;
+  
+parser.command('yml')
+  .help('creates a .travis.yml')
+  .callback(function (opts) {
+      createYML();
+  })
+  ;
+
+parser.command('hook')
+  .help('activate hook for this repo')
+  .callback(function (opts) {
+      activateHook();
+  })
+  ;
+  
+
+parser.parse();
