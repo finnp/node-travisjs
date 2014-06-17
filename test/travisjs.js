b24c7e4b47bcae5edd0a749da4e2aaddb4affe62
Finn <derfinn@gmail.com>
2014-06-17 23:25:34 +0200

    horrible hook tests

--- test/travisjs.js
+++ test/travisjs.js
@@ -1,6 +1,7 @@
 var nock = require('nock');
 var test = require('tape');
 var Travis = require('../lib/index.js');
+var assert = require('assert');
 
 var store = {
   set: function (key, value) {
@@ -8,23 +9,83 @@ var store = {
   },
   get: function (key) {
     return this[key];
-  },
-  token: 'testtoken'
+  }
 }
 
 var travisjs = new Travis(store);
 
+// ghauth mock (assumes it works correctly)
+travisjs._ghauth = function (opts, cb) {
+  assert.deepEqual(opts, {
+    scopes: [
+      "read:org",
+      "user:email",
+      "repo_deployment",
+      "repo:status",
+      "write:repo_hook"
+    ],
+    userAgent: 'node-travisjs',
+    note: 'node-travisjs',
+    noSave: true    
+  });
+  cb(null, {token: 'github_token'});
+}
+
 test('hook', function (t) {
-  t.plan(1);
-  var hookMock = nock('https://api.travis-ci.org')
-    .get('/repos/finnp/node-travisjs')
-    .reply(200, {repo: {id: 7}})
-    .put('/hooks')
-    .reply(200) // filter for the correct travis headers
-    ;
-  // turning on of travis hook
-  travisjs.hook(true);
-  setTimeout(function () {
-    t.ok(hookMock.isDone(), 'sufficient calls to travis-ci.org');
-  }, 50);
+  t.test('first time hook, github login', function (t) {   
+    t.plan(1);
+     
+    // Mocking Travis
+    var hookMock = nock('https://api.travis-ci.org', {
+        reqheaders: {
+          'Accept': 'application/vnd.travis-ci.2+json',
+          'User-Agent': 'node-travisjs'
+        }
+      })
+      
+      .post('/auth/github', {'github_token':'github_token'})
+      .reply(200, {'access_token':'travis_token'})
+      
+      .filteringPath(/\/repos\/[^&]*\/[^&]*/g, '/repos/user/repo')
+      .get('/repos/user/repo')
+      .reply(200, {repo: {id: 7}})
+      
+      .put('/hooks')
+      .reply(200) 
+      ;
+      
+    // turning on of travis hook
+    travisjs.hook(true);
+    setTimeout(function () {
+      hookMock.done(); // raises exceptions
+      t.ok(hookMock.isDone(), 'sufficient travis api calls');
+    }, 50);
+  });
+  
+  t.test('activating hook after login', function (t) {
+    t.plan(1);
+    
+    // Mocking Travis
+    var hookMock = nock('https://api.travis-ci.org', {
+        reqheaders: {
+          'Accept': 'application/vnd.travis-ci.2+json',
+          'User-Agent': 'node-travisjs'
+        }
+      })
+
+      .filteringPath(/\/repos\/[^&]*\/[^&]*/g, '/repos/user/repo')
+      .get('/repos/user/repo')
+      .reply(200, {repo: {id: 7}})
+      
+      .put('/hooks')
+      .reply(200) 
+      ;
+    
+    travisjs.hook(true);
+    setTimeout(function () {
+      hookMock.done(); // raises exceptions
+      t.ok(hookMock.isDone(), 'sufficient travis api calls');
+    }, 50);
+    
+  });
 });
